3b8588511ae45d477003039d2410bc006b1478d4
Blake Pell <blakepell@hotmail.com>
2018-12-07 13:12:22 -0500

    URL Encode and Decode

---
+++ StringTransforms/Code/Transforms/UrlDecode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Argus.Extensions;
+using StringTransforms.Models;
+
+namespace StringTransforms.Code.Transforms
+{
+    public class UrlDecode : BaseTransform
+    {
+        public override string Name { get; set; } = "URL Decode";
+
+        public override string Description { get; set; } = "Decodes a URL encoded string.";
+
+        public override string Identifier { get; set; } = "UrlDecode";
+
+        public override string Transform(string text)
+        {
+            return text.UrlDecode();
+        }
+
+    }
+}
---
+++ StringTransforms/Code/Transforms/UrlEncode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Argus.Extensions;
+using StringTransforms.Models;
+
+namespace StringTransforms.Code.Transforms
+{
+    public class UrlEncode : BaseTransform
+    {
+        public override string Name { get; set; } = "URL Encode";
+
+        public override string Description { get; set; } = "Encodes a string for use in a URL.";
+
+        public override string Identifier { get; set; } = "UrlEncode";
+
+        public override string Transform(string text)
+        {
+            return text.UrlEncode();
+        }
+
+    }
+}
