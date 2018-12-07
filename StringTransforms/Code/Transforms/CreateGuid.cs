5a6b7b614405fabc452e2d928068c68b37752301
Blake Pell <blakepell@hotmail.com>
2018-12-07 12:23:30 -0500

    Create GUID and SHA256 hash Transforms.

---
+++ StringTransforms/Code/Transforms/CreateGuid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringTransforms.Code.Transforms
+{
+    public class CreateGuid : BaseTransform
+    {
+        public override string Name { get; set; } = "Create GUID";
+
+        public override string Description { get; set; } = "Create a new GUID.";
+
+        public override string Identifier { get; set; } = "CreateGuid";
+
+        public override string Transform(string text)
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ StringTransforms/Code/Transforms/Sha256Hash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StringTransforms.Models;
+
+namespace StringTransforms.Code.Transforms
+{
+    public class Sha256Hash : BaseTransform
+    {
+        public override string Name { get; set; } = "SHA256 Hash";
+
+        public override string Description { get; set; } = "Creates the SHA256 hash for the specified string.";
+
+        public override string Identifier { get; set; } = "Sha256Hash";
+
+        public override string Transform(string text)
+        {
+            return Argus.Cryptography.HashUtilities.Sha256Hash(text);
+        }
+
+    }
+}
