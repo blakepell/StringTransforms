3176587871ea74a8717cef94044ce625a81132aa
Blake Pell <blakepell@hotmail.com>
2018-12-08 20:40:18 -0500

    vue-datetime references and test page that uses the component.

---
+++ StringTransforms/Pages/Test.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StringTransforms.Code.Transforms;
+using Argus.Extensions;
+using StringTransforms.Models;
+using StringTransforms.Code;
+
+namespace StringTransforms.Pages
+{
+    public class TestModel : BasePageModel
+    {
+        public TestModel()
+        {
+            this.ActiveMenu = "Home";
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
